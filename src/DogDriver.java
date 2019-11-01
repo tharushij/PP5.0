07a6008cc6d6d20ed96bdd7617f40699f5951340
tharushij <54290541+tharushij@users.noreply.github.com>
2019-11-01 13:33:27 -0700

    updated code

--- src/DogDriver.java
+++ src/DogDriver.java
@@ -2,6 +2,19 @@ public class DogDriver
 {
     public static void main (String[]args)
     {
+        Dog dog1 = new Dog("Fang", "Husky", 5, 80);
+        Dog dog2 = new Dog("Nutmeg", "Australian Sheepdog", 12, 45);
+        Dog dog3 = new Dog("Thea", "German Shepherd", 3, 60);
 
+        System.out.println("Fang's breed and age: ");
+        System.out.println(dog1.getBreed());
+        System.out.println(dog1.getAge());
+
+        System.out.println("Nutmeg's weight: ");
+        System.out.println(dog2.toKilos());
+
+        System.out.println("My dog's information: ");
+        System.out.println(dog3.toString());
+        // System.out.println(Dog.getCount());
     } //end method main
 } //end class DogDriver
